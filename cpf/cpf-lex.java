c3528a6e7d7549638df7167d727b56d51fe70185
root <leo-montiel@hotmail.com>
2021-04-25 00:44:42 +0000

    cpf works :D

---
+++ cpf/cpf-lex.java
@@ -0,0 +1,26 @@
+import java.util.*;
+%%
+%class cpf
+%standalone
+%line
+%column
+CPF = [0-9]{3}[\.][0-9]{3}[\.][0-9]{3}[\.][0-9]{2}
+
+%{
+ List<String> cpf = new ArrayList();
+%}
+
+%eof{
+  System.out.println("CPF: ");
+  for(int i=0;i<cpf.size();i++){
+    if(i==cpf.size()-1)
+     System.out.println(cpf.get(i));
+    else
+     System.out.println(cpf.get(i)+", ");
+  }
+%eof}
+
+%%
+{CPF} {cpf.add(yytext());}
+\n {}
+. {}
